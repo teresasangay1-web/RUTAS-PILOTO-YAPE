05b91948db19e65e1356a69d9caceb673ed943fa
teresasangay1-web <teresasangay1@gmail.com>
2025-11-26 23:59:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
 // Inicializar mapa
-const map = L.map('map').setView([-12.0464, -77.0428], 12); // centro por defecto
+const map = L.map('map').setView([-12.0464, -77.0428], 12);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Lista de gestores disponibles (nombres en minúscula)
-const gestores = ["pedro","katherine","juan"];
-
 // Leer query param ?gestor=nombre
-const urlParams = new URLSearchParams(window.location.search);
-const gestorParam = urlParams.get('gestor'); 
+let gestorParam = new URLSearchParams(window.location.search).get('gestor');
+if (gestorParam) gestorParam = decodeURIComponent(gestorParam).toLowerCase();
 
-if (gestorParam && gestores.includes(gestorParam.toLowerCase())) {
-    const jsonFile = `data/${gestorParam.toLowerCase()}.json`;
-    fetch(jsonFile)
-        .then(res => res.json())
-        .then(data => {
-            data.forEach(pt => {
-                L.marker([pt.Lat, pt.Lon])
-                 .bindPopup(`<b>${pt.Nombre}</b><br>${pt.Telefono}<br>${pt.Distrito}<br>${pt.Hexagono}<br>${pt.Fecha}`)
-                 .addTo(map);
-            });
+// Función para cargar todos los JSON disponibles desde /data
+async function loadGestorData(gestor) {
+    const jsonFile = `data/${gestor}.json`;
+    try {
+        const res = await fetch(jsonFile);
+        if (!res.ok) throw new Error("JSON no encontrado");
+        const data = await res.json();
+        data.forEach(pt => {
+            L.marker([pt.Lat, pt.Lon])
+             .bindPopup(`<b>${pt.Nombre}</b><br>${pt.Telefono}<br>${pt.Distrito}<br>${pt.Hexagono}<br>${pt.Fecha}`)
+             .addTo(map);
         });
+    } catch (err) {
+        alert("Gestor no encontrado o JSON faltante");
+        console.error(err);
+    }
+}
+
+if (gestorParam) {
+    loadGestorData(gestorParam);
 } else {
-    alert("Gestor no encontrado o no especificado en la URL");
+    alert("No se especificó gestor en la URL");
 }
